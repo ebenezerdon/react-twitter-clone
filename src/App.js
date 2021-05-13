85adb28544c6671e5bfede05859dabd6f2af1fb1
Ebenezer Don <ebenezerdonu@gmail.com>
2021-05-13 12:36:52 +0100

    use BrowserRouter to route components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import './App.css'
+import { Home, LeftPane, RightPane } from './components'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import './App.scss'
 
 const App = () => {
   return (
-   <></>
+    <BrowserRouter>
+       <div className="app">
+         <LeftPane />
+         <Switch>
+           <Route path="/">
+             <Home />
+           </Route>
+           <Route path="/profile">
+             Profile here
+           </Route>
+           <Route path="/messages">
+             Messages
+           </Route>
+         </Switch>
+         <RightPane />
+       </div>
+    </BrowserRouter>
   )
 }
 
