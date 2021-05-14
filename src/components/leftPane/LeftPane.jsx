67469fa0f03f7180191a69ed85821693dae5d681
Ebenezer Don <ebenezerdonu@gmail.com>
2021-05-14 10:06:10 +0100

    add leftPane content

--- src/components/leftPane/LeftPane.jsx
+++ src/components/leftPane/LeftPane.jsx
@@ -1,21 +1,57 @@
 import { NavLink } from 'react-router-dom'
+import { home, explore, twitter, notifications, messages, bookmarks, lists, profile, more } from './icons'
 import './leftPane.scss'
 
 const LeftPane = () => {
   return (
     <div className="left-pane">
-      <header>Twitter icon</header>
-      <nav>
-        <NavLink to="/home" activeClassName="selected">Home</NavLink>
-        <NavLink to="/explore" activeClassName="selected">Explore</NavLink>
-        <NavLink to="/notifications" activeClassName="selected">Notifications</NavLink>
-        <NavLink to="/messages" activeClassName="selected">Messages</NavLink>
-        <NavLink to="/bookmarks" activeClassName="selected">Bookmarks</NavLink>
-        <NavLink to="/lists" activeClassName="selected">Lists</NavLink>
-        <NavLink to="/profile" activeClassName="selected">Profile</NavLink>
-      </nav>
-      <button className="more">More</button>
-      <button className="tweet">Tweet</button>
+      <div className="container">
+        <header>{twitter}</header>
+
+        <nav>
+          <NavLink to="/" activeClassName="selected">
+            <span>{home} Home</span>
+          </NavLink>
+          <NavLink to="/explore" activeClassName="selected">
+            <span>{explore} Explore</span>
+          </NavLink>
+          <NavLink to="/notifications" activeClassName="selected">
+            <span>{notifications} Notifications</span>
+          </NavLink>
+          <NavLink to="/messages" activeClassName="selected">
+            <span>{messages} Messages</span>
+          </NavLink>
+          <NavLink to="/bookmarks" activeClassName="selected">
+            <span>{bookmarks} Bookmarks</span>
+          </NavLink>
+          <NavLink to="/lists" activeClassName="selected">
+            <span>{lists} Lists</span>
+          </NavLink>
+          <NavLink to="/profile" activeClassName="selected">
+            <span>{profile} Profile</span>
+          </NavLink>
+          <button className="more">
+            <span>{more} More</span>
+          </button>
+        </nav>
+
+        <button className="tweet">Tweet</button>
+
+        <footer>
+          <button className="account">
+            <div className="photo">
+              <img
+                alt="Ebenezer Don"
+                src="https://pbs.twimg.com/profile_images/1376379328070627332/2_7Gqbn0_normal.jpg"
+              />
+            </div>
+            <div>
+              <div className="name">Ebenezer Don</div>
+              <div className="username">@ebenezerDN</div>
+            </div>
+          </button>
+        </footer>
+      </div>
     </div>
   )
 }
