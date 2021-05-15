ce8a9437e058db032c154c4f7f5dbe379e0e60d2
Ebenezer Don <ebenezerdonu@gmail.com>
2021-05-15 21:05:44 +0100

    add other leftPane routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Home, LeftPane, RightPane } from './components'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Home, LeftPane, RightPane } from './components'
 import './App.scss'
 
 const App = () => {
@@ -8,15 +8,13 @@ const App = () => {
        <div className="app">
          <LeftPane />
          <Switch>
-           <Route path="/" exact>
-             <Home />
-           </Route>
-           <Route path="/profile">
-             Profile here
-           </Route>
-           <Route path="/messages">
-             Messages
-           </Route>
+           <Route path="/" component={Home} exact />
+           <Route path="/explore">Explore</Route>
+           <Route path="/notifications">Notifications</Route>
+           <Route path="/messages">Messages</Route>
+           <Route path="/bookmarks">Bookmarks</Route>
+           <Route path="/lists">Lists</Route>
+           <Route path="/profile">Profile</Route>
          </Switch>
          <RightPane />
        </div>
